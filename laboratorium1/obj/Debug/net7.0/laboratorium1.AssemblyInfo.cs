ffefcd989f06834c8052f90a6a97b25dee5aa185
Dominik Olechny <Dominik.Olechny_priv@outlook.com>
2023-11-02 18:20:20 +0100

    0.1

--- laboratorium1/obj/Debug/net7.0/laboratorium1.AssemblyInfo.cs
+++ laboratorium1/obj/Debug/net7.0/laboratorium1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LAB1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("laboratorium1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("LAB1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("LAB1")]
+[assembly: System.Reflection.AssemblyProductAttribute("laboratorium1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("laboratorium1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Wygenerowane przez klasę WriteCodeFragment programu MSBuild.
