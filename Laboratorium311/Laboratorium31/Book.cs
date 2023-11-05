01b7a29e662c5ef1b8e3bbf96007df42d7e61678
DominikOlechny <116365805+DominikOlechny@users.noreply.github.com>
2023-11-05 16:02:00 +0100

    Add files via upload

---
+++ Laboratorium311/Laboratorium31/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorium31
+{
+    internal class Book
+    {
+        public string title;
+        Person Author;
+        string dataWydania;
+    }
+    public book(string title, Person Author, string dataWydania)
+    {
+        this.title = title;
+        this.Author = Author;
+        this.dataWydania = dataWydania; 
+    }
+    public void View()
+    {
+        Console.WriteLine($"Imie:\t{FirstName}" + $"\tNazwisko:\t{LastName}" + $"\tWiek:\t{Wiek}");
+    }
+}
